cb979c8d657aaf7bcb40914d8a69851d7a4be73c
Tim W <twurman@umich.edu>
2015-03-22 12:44:47 -0400

    updated basic code

--- views.py
+++ views.py
@@ -1,11 +1,15 @@
 from flask import render_template, request
 from app import app
-from schedule_api import *
 
 @app.route('/')
 def index():
     options = {}
 
-    options['terms'] = get_terms()
-
     return render_template('index.html', **options)
+
+@app.route('/petinfo/<int:pet_id>')
+def getinfo(pet_id):
+	options = {}
+	options['pet_id'] = pet_id
+
+	return render_template('petinfo.html', **options)
