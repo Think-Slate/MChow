c6cf99450ce8a729b8d7a2854024d36424bb5d65
Tim W <twurman@umich.edu>
2015-03-22 15:39:00 -0400

    added sqllite db

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_MIGRATE_REPO'] = SQLALCHEMY_MIGRATE_REPO
+db = SQLAlchemy(app)
+
+class Fill(db.Model):
+	pet_id = db.Column(db.Integer, primary_key=True)
+	date_time = db.Column(db.DateTime)
+	water_fill = db.Column(db.Float)
+	food_fill = db.Column(db.Float)
+
+	def __init__(self, pet_id, water_fill, food_fill, date_time=None):
+		self.pet_id = pet_id
+		self.water_fill = water_fill
+		self.food_fill = food_fill
+		if date_time is None:
+			self.date_time = datetime.now()
+
+	def __repr__(self):
+		return '<Fill %d>' % self.pet_id
